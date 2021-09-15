66cdb9afe2c684e2b5454c049168a48d85e82ee4
KingPEPSALT <quinmackfoster+github@gmail.com>
2021-09-16 00:45:56 +0100

    Added search functionality to play command

--- commands/voice/play.js
+++ commands/voice/play.js
@@ -1,45 +1,68 @@
 const Discord = require("discord.js");
-const ytdl = require('ytdl-core-discord');
-const {getInfo} = require('ytdl-getinfo')
-const SongInformation = require('../../song_objects/SongInformation');
-const GuildMusicController = require('../../song_objects/GuildMusicController');
+const ytdl = require("ytdl-core-discord");
+const { getInfo } = require("ytdl-getinfo");
+const SongInformation = require("../../song_objects/SongInformation");
+const GuildMusicController = require("../../song_objects/GuildMusicController");
+const ytsearch = require("youtube-search-api");
 
 module.exports = {
-    name:'play',
-    alias:['summon','join', 'p', 'connect'],
-    description: 'Plays a youtube video in the voice chat you are currently in.',
-    args: true,
-    usage: '<url>',
-    guildOnly: true,
-    async execute(msg, args){
-        if(!msg.member.voice.channel) return msg.channel.send({embed:{color:0xe83f3f, description:'You aren\'t in a voice channel.'}});
-        await msg.member.voice.channel.join();
+  name: "play",
+  alias: ["summon", "join", "p", "connect"],
+  description: "Plays a youtube video in the voice chat you are currently in.",
+  args: true,
+  usage: "<url>",
+  guildOnly: true,
+  async execute(msg, args) {
+    if (!msg.member.voice.channel)
+      return msg.channel.send({
+        embed: {
+          color: 0xe83f3f,
+          description: "You aren't in a voice channel.",
+        },
+      });
+    await msg.member.voice.channel.join();
+    var getInfoParam = args[0];
+    console.log(args.join(" "));
+    if (!(await ytdl.validateURL(getInfoParam)))
+      getInfoParam = await ytsearch
+        .GetListByKeyword(args.join(" "))
+        .then((result) => {
+          return result.items[0].id;
+        });
 
-
-        const song = new SongInformation(await ytdl.getInfo(args[0]), msg.author)
-        if(!msg.client.queues.has(msg.guild)){ 
-            msg.client.queues.set(msg.guild, new GuildMusicController(msg.guild.voice, [song]))
-        }
-        else {
-            msg.client.queues.get(msg.guild).enqueue(song)
-        }
-        
-        const playing_message = await msg.channel.send({embed:{
-            color:0x22e34c,
-            fields:[
-                {
-                    name: '♪ Playing ♪',
-                    value: `[${song.title} - ${song.channel}](${song.url})`
-                },
-                {
-                    name:'\u200B',
-                    value: `\`${song.formattedLength()} | Requested by ${song.requester.tag}\``,
-                    inline: true
-                }
-            ],
-            thumbnail:song.image
-        }});
-        msg.delete();        
-        return playing_message;
+    const song = new SongInformation(
+      await ytdl.getInfo(getInfoParam),
+      msg.author
+    );
+    if (!msg.client.queues.has(msg.guild)) {
+      msg.client.queues.set(
+        msg.guild,
+        new GuildMusicController(msg.guild.voice, [song])
+      );
+    } else {
+      msg.client.queues.get(msg.guild).enqueue(song);
     }
-}
+
+    const playing_message = await msg.channel.send({
+      embed: {
+        color: 0x22e34c,
+        fields: [
+          {
+            name: "♪ Playing ♪",
+            value: `[${song.title} - ${song.channel}](${song.url})`,
+          },
+          {
+            name: "\u200B",
+            value: `\`${song.formattedLength()} | Requested by ${
+              song.requester.tag
+            }\``,
+            inline: true,
+          },
+        ],
+        thumbnail: song.image,
+      },
+    });
+    msg.delete();
+    return playing_message;
+  },
+};
--- song_objects/GuildMusicController.js
+++ song_objects/GuildMusicController.js
@@ -1,73 +1,85 @@
-const ytdl = require('ytdl-core-discord')
+const { exceptions } = require("winston");
+const ytdl = require("ytdl-core-discord");
+module.exports = class GuildMusicController {
+  constructor(voiceState, queue) {
+    this.voice = voiceState;
+    this.queue = queue || [];
+    this.paused = queue.length == 0 ? true : false;
+    if (!this.paused) this.playTop();
+  }
 
-module.exports = class GuildMusicController{
+  async playTop() {
+    console.log(this.queue[0]);
+    const dispatcher = this.voice.connection.play(
+      await ytdl(this.queue[0].url).catch((e) => {
+        throw new Error("Invalid URL: " + this.queue[0].url + " | " + e);
+      }),
+      { type: "opus", highWaterMark: 100, volume: false }
+    );
+    if (this.paused) this.pause();
+    dispatcher.once("finish", () => {
+      if (this.queue.length == 0) return dispatcher.destroy();
+      this.skip();
+    });
+  }
 
-    constructor(voiceState, queue){
-        this.voice = voiceState;
-        this.queue = queue || [];
-        this.paused = queue.length==0 ? true : false;
-        if(!this.paused) this.playTop();
+  skip() {
+    if (this.queue.length == 0) return "The queue is empty.";
+    this.queue.shift();
+    if (!this.queue.length == 0) {
+      this.playTop();
     }
+  }
 
-    async playTop(){
-        console.log(this.queue[0])
-        const dispatcher = this.voice.connection.play(await ytdl(this.queue[0].url).catch(e=>{
-            return 'Invalid URL.';
-        }), {type: 'opus', highWaterMark:100, volume:false});
-        if(this.paused) this.pause();
-        dispatcher.once('finish', ()=>{
-            if(this.queue.length==0) return dispatcher.destroy();
-            this.skip();
-        });
-    }
+  enqueue(song) {
+    this.queue.push(song);
+    if (this.queue.length == 1) this.playTop();
+  }
 
-    skip(){
-        if(this.queue.length == 0) return 'The queue is empty.';
-        this.queue.shift();
-        if(!this.queue.length == 0){ this.playTop()}
-    }
+  remove(idx) {
+    if (idx >= this.queue.length || idx < 1) return "That is an invalid song.";
+    this.queue.splice(1, idx);
+  }
 
-    enqueue(song){
-        this.queue.push(song);
-        if(this.queue.length == 1) this.playTop();
-    }
+  clear() {
+    this.queue = [this.queue[0]];
+  }
 
-    remove(idx){
-        if(idx >= this.queue.length || idx < 1) return 'That is an invalid song.'
-        this.queue.splice(1, idx);
-    }
+  nowPlaying() {
+    return this.queue[0] || null;
+  }
 
-    clear(){
-        this.queue = [this.queue[0]]
-    }
+  pause() {
+    if (!this.nowPlaying()) return "Nothing is playing.";
+    if (this.paused) return "It is already paused.";
+    this.voice.connection.dispatcher.pause(true);
+    this.paused = true;
+  }
 
-    nowPlaying(){
-        return this.queue[0] || null;
-    }
+  resume() {
+    if (!this.nowPlaying()) return "Nothing is playing.";
+    if (!this.paused) return "It isn't paused.";
+    this.voice.connection.dispatcher.resume();
+    this.paused = false;
+  }
 
-    pause(){
-        if(!this.nowPlaying()) return 'Nothing is playing.';
-        if(this.paused) return 'It is already paused.';
-        this.voice.connection.dispatcher.pause(true);
-        this.paused = true;
-    }
+  currentElapsedTime() {
+    if (!this.nowPlaying()) return "Nothing is playing.";
+    return (
+      this.voice.connection.dispatcher.streamTime -
+      this.voice.connection.dispatcher.pausedTime
+    );
+  }
 
-    resume(){
-        if(!this.nowPlaying()) return 'Nothing is playing.';
-        if(!this.paused) return 'It isn\'t paused.';
-        this.voice.connection.dispatcher.resume();
-        this.paused = false;
-    }
-
-    currentElapsedTime(){
-        if(!this.nowPlaying()) return 'Nothing is playing.';
-        return this.voice.connection.dispatcher.streamTime - this.voice.connection.dispatcher.pausedTime;
-    }
-
-    displayQueue(){
-        if(this.queue.length == 0) return 'The queue is empty.';
-        return this.queue.map((song, position)=>`${position+1} | ${song.title} - ${song.channel} | Length: ${song.formattedLength()}`).join('\n');
-    }
-
-}
-
+  displayQueue() {
+    if (this.queue.length == 0) return "The queue is empty.";
+    return this.queue
+      .map(
+        (song, position) =>
+          `${position + 1} | ${song.title} - ${
+            song.channel
+          } | Length: ${song.formattedLength()}`
+      )
+      .join("\n");
+  }
+};
